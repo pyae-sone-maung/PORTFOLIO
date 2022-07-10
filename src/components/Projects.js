9f798466ce9e1b3091577f8063b49a7fcadb5a29
pyae-sone-maung <pyaesonemaung.dec@gmail.com>
2022-07-10 23:13:11 +0630

    modify project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,36 @@
 import React from 'react'
-import projectsInfo from '../utils/data'
+import projectsInfo from '../utils/project-data'
 
 const Projects = () => {
-  return (
-     <div className='py-5 grid place-content-center bg-purple-400'>
-          <div className='px-0 py-5'>
-               <p className='text-2xl font-bold text-center uppercase'> Featured Projects </p>
-          </div>
-
-          <div className='px-1 py-5 gap-3 gap-x-10 lg:px-14 grid lg:grid-cols-2'>
-               {
-                    projectsInfo.map(project => (
-                              <div className='px-5 py-5 rounded-md drop-shadow-md bg-gray-300 hover:bg-blue-500'>
+     return (
+          <div className='py-2 mb-8 grid place-content-center' id='projects'>
+               <div className='px-0 py-5 grid place-items-center'>
+                    <p className='text-2xl text-white font-bold text-center uppercase'> Featured Projects </p>
+                    <div className=" w-64 mt-2 border-b-4 border-dotted lg:hidden"> </div>
+                    <div className='w-64  mt-2 h-0.5 skill-bg-color rounded-full hidden lg:block'> </div>
+               </div>
+               <div className='px-1 py-5 lg:-mt-3 gap-3 gap-x-7 text-white lg:px-14 grid lg:grid-cols-2'>
+                    {
+                         projectsInfo.map(project => (
+                              <div key={project.id} className='px-4 py-5 w-96 rounded-md drop-shadow-md glassbg'>
                                    <h2 className='py-1 text-xl font-bold'> {project.title} </h2>
-                                   <div className='py-1 flex gap-1 text-white text-xs'>
-                                        {project.tech_stack.map(tech => (
-                                             <span className='px-4 py-1 bg-gray-800 rounded-full'> {tech} </span>
-                                        ) )}
+                                   <div className='py-1 grid grid-cols-4 gap-1 text-center text-white text-xs'>
+                                        {project.tech_stack.map((tech, index) => (
+                                             <span key={index} className='px-4 py-1 bg-gray-800 rounded-full'> {tech} </span>
+                                        ))}
                                    </div>
-                                   <p className='py-1 mt-2 text-base'> {project.description}</p>
-                                   <div className='py-1 flex gap-4 drop-shadow-md'>
-                                        <a href={project.link1} className='px-5 py-2 text-xs text-white bg-blue-500 rounded-md'> View Source </a>
-                                        <a href={project.link2} className='px-5 py-2 text-xs text-white bg-blue-500 rounded-md'> Demo </a>
+                                   <p className='py-1 mt-2 text-sm text-justify'> {project.description}</p>
+                                   <div className='py-0 flex gap-4 drop-shadow-md'>
+                                        {project.links.map(pj => (
+                                             <a key={pj.id} href={pj.link} className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 font-bold px-5 py-2 mt-5 text-xs text-white bg-indigo-500 rounded-md'> {pj.name} </a>
+                                        ))}
                                    </div>
                               </div>
-                    ))
-               }
+                         ))
+                    }
+               </div>
           </div>
-     </div>
-  )
+     )
 }
 
 export default Projects
