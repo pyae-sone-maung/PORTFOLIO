def1afb7c9e6d0de99d195d172aaeb3a1d771ad2
pyae-sone-maung <pyaesonemaung.dec@gmail.com>
2022-07-10 23:11:08 +0630

    update about info

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,24 @@
 import React from "react";
-import about_pic from "../assets/images/Me.jpg";
+import about_pic from '../assets/images/Profile2.jpg'
 
 const About = () => {
     return (
-        <div className="py-4 grid place-items-center bg-blue-200">
-            <div className="flex lg:space-x-10">
-                <div className="py-5 px-10 hidden lg:block">
-                    <img
-                        src={about_pic}
-                        alt="Profile"
-                        className="w-56 h-56 rounded-lg"
-                    />
+        <div className="py-4 mt-5 lg:mt-0 grid place-content-center" id="about">
+            <div className="container py-5 grid lg:grid-cols-2 lg:place-content-center glassbg drop-shadow-lg">
+                <div className="py-5 mx-auto hidden lg:block">
+                    <img src={about_pic} alt="Profile" className="w-56 h-56 about-pic" />
                 </div>
-                <dir className="px-0 text-center">
-                    <p className="text-xl font-bold lg:text-left mb-4 uppercase">
-                        {" "}
-                        About Me{" "}
+                <div className="px-0 lg:-ml-16 lg:mt-2 text-center text-white drop-shadow">
+                    <p className="text-2xl font-bold lg:text-left lg:px-5 mb-4 uppercase"> About Me </p>
+                    <p className="px-5 text-justify">
+                        I am Pyae Sone Maung. I've got a bachelor degree of
+                        computer science from the University Of Computer
+                        Studies, Sittway. I am very interested in web
+                        application development. I have been three months
+                        intership as a junior web developer at BEE Data Myanmar
+                        Software Solutions, MICT Park, Yangon.
                     </p>
-                    <p className="lg:text-justify">
-                        {" "}
-                        I am Pyae Sone Maung. I've got bachelor degree of <br />
-                        computer science from the university of computer studies
-                        sittway.
-                    </p>
-                </dir>
+                </div>
             </div>
         </div>
     );
