6c3cfd4d5422ccf728c3b66ce6060267db5aaa48
pyae-sone-maung <pyaesonemaung.dec@gmail.com>
2022-07-10 23:15:07 +0630

    adding skill data list

---
+++ src/utils/skill-items.js
@@ -0,0 +1,83 @@
+import html from "../assets/logo/html.png";
+import css from "../assets/logo/css.png";
+import bootstrap from "../assets/logo/bootstrap.png";
+import javascript from "../assets/logo/javascript.jpg";
+import jquery from "../assets/logo/jquery.png";
+import git from "../assets/logo/git.jpg";
+import tailwind from "../assets/logo/tailwindcss.jpg";
+import reactjs from "../assets/logo/react.png";
+import nodejs from "../assets/logo/nodejs.png";
+import expressjs from "../assets/logo/express.jpg";
+import mongodb from "../assets/logo/mongodb.jpg";
+import postgresql from "../assets/logo/postgresql.jpg";
+import mysql from "../assets/logo/mysql.png";
+
+const Skill_Items = [
+    {
+        id: 1,
+        name: "HTML",
+        icon: html,
+    },
+    {
+        id: 2,
+        name: "CSS",
+        icon: css,
+    },
+    {
+        id: 3,
+        name: "Bootstrap",
+        icon: bootstrap,
+    },
+    {
+        id: 4,
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        id: 5,
+        name: "jQuery",
+        icon: jquery,
+    },
+    {
+        id: 6,
+        name: "Git",
+        icon: git,
+    },
+    {
+        id: 7,
+        name: "Tailwind",
+        icon: tailwind,
+    },
+    {
+        id: 8,
+        name: "React",
+        icon: reactjs,
+    },
+    {
+        id: 9,
+        name: "Node",
+        icon: nodejs,
+    },
+    {
+        id: 10,
+        name: "Express",
+        icon: expressjs,
+    },
+    {
+        id: 11,
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        id: 12,
+        name: "PostgreSQL",
+        icon: postgresql,
+    },
+    {
+        id: 13,
+        name: "MySQL",
+        icon: mysql,
+    },
+];
+
+export default Skill_Items;
