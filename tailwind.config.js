badd377f3302a1d32b22e2eb5f684195044051e5
pyae-sone-maung <pyaesonemaung.dec@gmail.com>
2022-07-10 23:15:35 +0630

    modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     content: ["./src/**/*.{js, jsx, ts, tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "navy-dark-blue": "#0B0B45",
+                "dark-sky": "#070058",
+            },
+        },
     },
     plugins: [],
 };
