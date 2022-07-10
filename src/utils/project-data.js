488bfd146dc814c259d534316abd53aef46b6059
pyae-sone-maung <pyaesonemaung.dec@gmail.com>
2022-07-10 23:14:49 +0630

    project data list

---
+++ src/utils/project-data.js
@@ -0,0 +1,70 @@
+const projectInfo = [
+    {
+        id: 1,
+        title: "Transportation Services API",
+        description:
+            "Backend API that collects data on some of the transportations services in Taungup city, Rakhine State.",
+        tech_stack: ["Node", "Express", "MongoDB", "Cloudinary"],
+        links: [
+            {
+                id: 1,
+                name: "Source Code",
+                link: "https://github.com/pyae-sone-maung/transportation-services-api",
+            },
+            {
+                id: 2,
+                name: "Demo",
+                link: "https://arakan-tg-transportation-api.herokuapp.com/",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Google Sign In App",
+        description:
+            "Sample google authentication app using passport.js and tailwind css.",
+        tech_stack: ["Node", "Express", "Passport", "EJS"],
+        links: [
+            {
+                id: 1,
+                name: "Source Code",
+                link: "https://github.com/pyae-sone-maung/Google-Authentication-Node.js-App",
+            },
+            {
+                id: 2,
+                name: "Demo",
+                link: "https://google-profile.herokuapp.com/"
+            }
+        ],
+    },
+    {
+        id: 3,
+        title: "PORTFOLIO",
+        description:
+            "This is my portfolio website ",
+        tech_stack: ["React", "Tailwind", "EmailJs"],
+        links: [
+            {
+                id: 1,
+                name: "Source Code",
+                link: "https://github.com/pyae-sone-maung/PORTFOLIO",
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Registration System API",
+        description:
+            "This project is a sample registration system with email verification.",
+        tech_stack: ["Node", "Express", "MongoDB"],
+        links: [
+            {
+                id: 1,
+                name: "Source Code",
+                link: "https://github.com/pyae-sone-maung/registration-and-login-node.js-api",
+            }
+        ]
+    },
+];
+
+export default projectInfo;
